dd87f1d15ed3a4ab6b8c31fd4fe36bafdea69935
snowgoing <snowgoing@gmail.com>
2017-08-01 14:28:16 -0700

    Test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 console.log('Mongo', process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true});
 
 module.exports = { mongoose };
