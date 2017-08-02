321e558cac2109afec94edebce4410cb5ea7cfe9
snowgoing <snowgoing@gmail.com>
2017-08-01 18:41:09 -0700

    Test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
+const options = {
+    useMongoClient: true,
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
 console.log('Mongo', process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {
-  useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, options);
 
 module.exports = { mongoose };
